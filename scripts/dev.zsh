da89266e2b328f7f78123b125382926f58bcb5d9
Marcus Westin <marcus.westin@gmail.com>
2022-08-26 19:03:18 -0400

    Add script to open vscode instances of all the relevant directories

---
+++ scripts/dev.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh # Boilerplate snippet - go to root, include aldyn-lib.sh & return to script dir
+WD=`pwd` && cd "$(git rev-parse --show-toplevel)" && . scripts/setup/boilerplate.zsh
+
+code apps/bun-server
+code apps/expo-hugson-palleys
+code scripts
