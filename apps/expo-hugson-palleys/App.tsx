e87dd4fddc905c2722023d6ea5f082e934b35ecc
Marcus Westin <marcus.westin@gmail.com>
2022-08-29 10:13:28 -0400

    Start using a native-base expo app

--- apps/expo-hugson-palleys/App.tsx
+++
@@ -1,5 +0,0 @@
-import TestScreen from "./src/screens/TestScreen"
-
-export default function App() {
-  return <TestScreen />
-}
---
+++ apps/mobile-expo-app/App.tsx
@@ -0,0 +1,94 @@
+import {
+  Box,
+  Button,
+  Center,
+  extendTheme,
+  Heading,
+  HStack,
+  Link,
+  NativeBaseProvider,
+  Switch,
+  Text,
+  useColorMode,
+  VStack,
+} from "native-base"
+import React from "react"
+import NativeBaseIcon from "./components/NativeBaseIcon"
+
+import { TestAPI } from "./src/shared/universal/apis/api-specs"
+import { makeAPIClient } from "./src/shared/universal/apis/lib-api/lib-api-specification"
+
+const client = makeAPIClient(TestAPI)
+
+// Define the config
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+}
+
+// extend the theme
+export const theme = extendTheme({ config })
+type MyThemeType = typeof theme
+declare module "native-base" {
+  interface ICustomTheme extends MyThemeType {}
+}
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Center _dark={{ bg: "blueGray.900" }} _light={{ bg: "blueGray.50" }} px={4} flex={1}>
+        <VStack space={5} alignItems="center">
+          <NativeBaseIcon />
+          <Heading size="lg">Welcome to NativeBase</Heading>
+          <HStack space={2} alignItems="center">
+            <Text>Edit</Text>
+            <Box
+              _web={{
+                _text: {
+                  fontFamily: "monospace",
+                  fontSize: "sm",
+                },
+              }}
+              px={2}
+              py={1}
+              _dark={{ bg: "blueGray.800" }}
+              _light={{ bg: "blueGray.200" }}
+            >
+              App.js
+            </Box>
+            <Text>and save to reload.</Text>
+          </HStack>
+          <Link href="https://docs.nativebase.io" isExternal>
+            <Text color="primary.500" underline fontSize="xl">
+              Learn NativeBase now
+            </Text>
+          </Link>
+          <Button
+            onPress={async () => {
+              let res = await client.ping({ message: "hi there" })
+              alert(res.message)
+            }}
+          >
+            Ping server
+          </Button>
+          <ToggleDarkMode />
+        </VStack>
+      </Center>
+    </NativeBaseProvider>
+  )
+}
+
+// Color Switch Component
+function ToggleDarkMode() {
+  const { colorMode, toggleColorMode } = useColorMode()
+  return (
+    <HStack space={2} alignItems="center">
+      <Text>Dark</Text>
+      <Switch
+        isChecked={colorMode === "light"}
+        onToggle={toggleColorMode}
+        aria-label={colorMode === "light" ? "switch to dark mode" : "switch to light mode"}
+      />
+      <Text>Light</Text>
+    </HStack>
+  )
+}
