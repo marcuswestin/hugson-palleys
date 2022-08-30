dcd4642d27dd3a15e9229bfd6389280fcb40ce77
Marcus Westin <marcus.westin@gmail.com>
2022-08-30 07:48:32 -0400

    Move old scripts

--- scripts/run/mobile-expo-app.zsh
+++ scripts/run/mobile-expo-app.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh # Boilerplate snippet - go to root, include aldyn-lib.sh & return to script dir
 WD=`pwd` && cd "$(git rev-parse --show-toplevel)" && . scripts/setup/boilerplate.zsh
 
-cd ${ROOT}/apps/expo-hugson-palleys
+cd ${ROOT}/apps/mobile-expo-app
 
 expo start --ios # --android
