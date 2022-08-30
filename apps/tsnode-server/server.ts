5656749b279dec4e516863134d088cd27edf6e05
Marcus Westin <marcus.westin@gmail.com>
2022-08-30 10:36:18 -0400

    Start logging server requests to console

--- apps/tsnode-server/server.ts
+++ apps/tsnode-server/server.ts
@@ -5,6 +5,7 @@ import { config } from "./src/shared/universal/config"
 console.log("Start server at", config.server.getURL())
 
 let server = http.createServer((req, res) => {
+  console.log("REQ:", req.url)
   TestAPI.handleRequest(req, res)
 })
 server.listen(config.server.port)
